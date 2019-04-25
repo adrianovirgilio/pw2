7f3be18a55c4f206f9ca4e19b2264b43c13a2a7a
adrianovirgilio <49097002+adrianovirgilio@users.noreply.github.com>
2019-04-25 19:59:33 -0300

    Add files via upload

--- form_busca.php
+++ form_busca.php
@@ -3,9 +3,19 @@
 <head>
 <meta charset="utf-8">
 <title>Formulário de pesquisa</title>
+<link href="css/estilo.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+<div id="todoconteudo">
+	
+    <div id="cabecalho">    
+    </div>
+    
+    <div id="menu">    
+    </div>
+    
+    <div id="corpo">
 	<fieldset>
     <legend>Formulário de Pesquisa</legend>
 	<form action="#" method="get">
@@ -24,17 +34,42 @@
 				print "<p>Resultado:</p>";
 				while($registro = $consulta->fetch(PDO::FETCH_ASSOC))
 					{
-						print "<p>{$registro['matricula']} - {$registro['nome']}</p>";
+						print "<p>
+						    {$registro['matricula']} - {$registro['nome']}
+							<a href='form_busca.php?excluir&matricula={$registro['matricula']}'>
+								<img src='icones/excluir.png' title='Excluir registro'></a>								 
+						    </p>";
 					}
 											
 				}catch(PDOException $e){
 					print "Erro ocorrido:" . $e->getMessage();					
 					}
 		
-		}	
-	?>
-    
-    
+		}
+		else if(isset($_GET['excluir']))
+		{	
+			$matricula = $_GET['matricula'];
+			$con = new PDO('mysql:host=localhost:3307;dbname=banco_apm','root','usbw');		
+			$comando_sql = "DELETE FROM tabela_professores WHERE matricula = :valor";
+			$stmt = $con->prepare($comando_sql);
+			$stmt->bindParam(':valor',$matricula);
+			$stmt->execute();
+			$rs = $stmt->rowCount();	
+			if($rs)
+			{
+				echo "<script>alert('Registro apagado com sucesso!');</script>";	
+			}else{
+				echo "<script>alert('Não foi possível excluir!');</script>";		
+			}
+			
+			
+		}
+	?>    
     </fieldset>
+    </div> 
+         
+    <div id="rodape">
+    </div>
+</div>
 </body>
 </html>
